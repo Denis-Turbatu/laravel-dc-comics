66110d53e8cf4e7ce64279d2f122fe7bbeae7364
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-14 16:56:13 +0200

    feat: add bootstrap classes for styling

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,8 +2,39 @@
 
 @section('content')
     <h1>Lista Comics</h1>
-
-    @foreach ($listaComics as $comic)
-        <p>{{$comic['title']}}</p>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($listaComics as $comic)
+                <div class="card border border-0 m-4" style="width: 17rem;">
+                    <img src="{{ asset($comic['thumb']) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Titolo:
+                            <span class="fw-semibold">
+                                {{ $comic['title'] }}
+                            </span>
+                        </p>
+                        <p class="card-text mb-0">
+                            Serie:
+                            <span class="fw-semibold">
+                                {{ $comic['series'] }}
+                            </span>
+                        </p>
+                        <p class="card-text">
+                            Data di uscita:
+                            <span class="fw-semibold">
+                                {{ $comic['sale_date'] }}
+                            </span>
+                        </p>
+                        <p class="card-text">
+                            Prezzo:
+                            <span class="fw-semibold">
+                                {{ $comic['price'] }}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
