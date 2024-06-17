9841b2de827d89606ba7701b13cb52ce5f7edb59
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-17 16:09:40 +0200

    feat: add edit.blade.php + apply route for edit in index.blade.php + apply edit return for ComicController

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="my-5">Aggiungi un comics!</h1>
+
+        <form action="{{route('comics.update', ['comic' => $comic->id])}}" method="POST">
+            {{-- Cookie per far riconoscere il form al server --}}
+            @csrf
+            {{-- Dichiarazione per usare metodo PUT --}}
+            @method('PUT')
+
+            <div class="mb-3">
+                <label for="title" class="form-label fw-semibold">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="formFile" class="form-label fw-semibold">Immagine (Opzionale)</label>
+                <input class="form-control" type="file" id="formFile">
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label fw-semibold">Serie</label>
+                <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date" class="form-label fw-semibold">Data di pubblicazione</label>
+                <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label fw-semibold">Tipo</label>
+                <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label fw-semibold">Prezzo</label>
+                <input type="text" class="form-control" id="price" name="price" value="{{$comic->price}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label fw-semibold">Descrizione</label>
+                <textarea class="form-control" id="description" name="description" rows="6">{{$comic->description}}"</textarea>
+            </div>
+
+            <button class="btn btn-success" type="submit">Salva</button>
+        </form>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,7 +6,7 @@
 @endphp
     
     <div class="container">
-        <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between align-items-center my-5">
             <h1>Lista Comics</h1>
             <a href="{{route("comics.create")}}" class="px-4 py-2 bg-success text-decoration-none text-white rounded-2">Crea</a>
         </div>
@@ -40,8 +40,9 @@
                             </span>
                         </p>
                         <div>
-                            <a href="">Modifica</a>
-                            <a href="">Elimina</a>
+                            <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}" class="text-decoration-none text-white bg-primary p-2 rounded-2">Modifica</a>
+                            <a href="" class="
+                            text-decoration-none text-white bg-danger p-2 rounded-2">Elimina</a>
                         </div>
                     </div>
                 </div>
